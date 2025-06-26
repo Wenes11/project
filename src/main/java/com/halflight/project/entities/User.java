2a51a59d4ea9931089aa231de8a7b321087038e3
Wenes11 <wenes.vargas10@gmail.com>
2025-06-26 03:11:37 -0300

    Criação de User

---
+++ src/main/java/com/halflight/project/entities/User.java
@@ -0,0 +1,80 @@
+package com.halflight.project.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    //criando as variaveis;
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String email;
+    private String telefone;
+    private String senha;
+
+    public User(){
+    }
+ // criando o construtor;
+    public User(Long id, String name, String email, String telefone, String senha) {
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.telefone= telefone;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+}
---
+++ src/main/java/com/halflight/project/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.halflight.project.resources;
+
+import com.halflight.project.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findALL() {
+        User u =  new User(1L, "Maria", "mairia@gmail.com","64992486472","jv161820");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
