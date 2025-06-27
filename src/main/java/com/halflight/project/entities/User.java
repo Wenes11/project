0e6bf4bca707d964b9374fb6c70d6368159f5fc1
Wenes11 <wenes.vargas10@gmail.com>
2025-06-26 22:49:09 -0300

    Criação de H2 e properrties

--- src/main/java/com/halflight/project/entities/User.java
+++ src/main/java/com/halflight/project/entities/User.java
@@ -1,17 +1,26 @@
 package com.halflight.project.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import javax.annotation.processing.Generated;
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
+@Table(name="tb-user")
 public class User implements Serializable {
     //criando as variaveis;
     private static final long serialVersionUID = 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String telefone;
     private String senha;
+
     public User(){
     }
  // criando o construtor;
--- src/main/java/com/halflight/project/resources/UserResource.java
+++ src/main/java/com/halflight/project/resources/UserResource.java
@@ -5,11 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// Aqui é um criação de rotas.
 @RestController
 @RequestMapping(value = "/users")
+
 public class UserResource {
-    @GetMapping
+    @GetMapping("/usuarios")
     public ResponseEntity<User> findALL() {
         User u =  new User(1L, "Maria", "mairia@gmail.com","64992486472","jv161820");
         return ResponseEntity.ok().body(u);
